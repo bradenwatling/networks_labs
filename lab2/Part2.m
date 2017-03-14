abfc2aac3bb168d571f07231cc7babb6026909ea
Braden Watling <braden.watling@mail.utoronto.ca>
2017-03-14 18:04:14 -0400

    Finished lab2

--- lab2/Part2.m
+++ lab2/Part2.m
@@ -1,6 +1,18 @@
 [poisson_i, poisson_time, poisson_size] = textread('poisson3.data', '%f %f %f');
 [trafficsink_i, trafficsink_time, trafficsink_size] = textread('trafficsink.data', '%f %f %f');
+[trafficsink_2_i, trafficsink_2_time, trafficsink_2_size] = textread('trafficsink_ug61_to_ug248.data', '%f %f %f');
 
+diff = 0;
+diff_2 = 0;
+
+for i = 1 : length(poisson_arrivals)
+    diff = diff - trafficsink_time(i) + poisson_time(i) - poisson_time(1);
+    diff_2 = diff_2 - trafficsink_2_time(i) + poisson_time(i) - poisson_time(1);
+end
+
+avg_diff = diff / length(poisson_arrivals)
+avg_diff_2 = diff_2 / length(poisson_arrivals)
+return
 poisson_sum = 0;
 trafficsink_sum = 0;
 
