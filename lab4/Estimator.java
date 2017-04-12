ad2b3ba768abef363c548e873ca192734dcd6781
Braden Watling <braden.watling@mail.utoronto.ca>
2017-04-12 16:14:44 -0400

    Reorganized Estimator so it can be called by another class iteratively. Implemented maximum backlog algorithm, improved matlab script

--- lab4/Estimator.java
+++ lab4/Estimator.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 import java.nio.ByteBuffer;
 import java.util.concurrent.CountDownLatch;
 
@@ -13,7 +14,17 @@ public class Estimator {
     long receiveTime;
   }
 
-  private static void packetTrain(PacketData[] data, InetAddress targetAddr, int numPackets, int packetSize, int packetRate, short targetPort, short receivePort) {
+  private PacketData[] packetData;
+  private InetAddress targetAddr;
+  private short targetPort;
+  private short receivePort;
+  private int numPackets;
+  private int packetSize;
+  private int packetRate;
+
+  private CountDownLatch latch;
+
+  private void packetTrain() {
     packetSize = Math.min(MAX_PACKET_SIZE, packetSize);
 
     // Sending packets of size packetSize bytes at packetRate kbps. The delay between them in nanoseconds is
@@ -31,10 +42,10 @@ public class Estimator {
         bb.putInt(i);
 
         // Create the PacketData
-        data[i] = new PacketData();
-        data[i].seqNo = i;
+        packetData[i] = new PacketData();
+        packetData[i].seqNo = i;
         // Record the time that we sent this packet
-        data[i].sendTime = System.nanoTime();
+        packetData[i].sendTime = System.nanoTime();
 
         // Send the packet to the target
         DatagramPacket d = new DatagramPacket(buf, buf.length, targetAddr, targetPort);
@@ -49,7 +60,7 @@ public class Estimator {
     }
   }
 
-  private static void receivePackets(final PacketData[] data, final CountDownLatch latch, final int numPackets, final short receivePort) {
+  private void receivePackets() {
     new Thread() {
       @Override
       public void run() {
@@ -67,7 +78,7 @@ public class Estimator {
             bb.getShort();
             int seqNo = bb.getInt();
             // Record the time that we received this packet
-            data[seqNo].receiveTime = System.nanoTime();
+            packetData[seqNo].receiveTime = System.nanoTime();
 
             // Count down the latch
             latch.countDown();
@@ -77,6 +88,98 @@ public class Estimator {
         }
       }
     }.start();
+  }
+
+  public Estimator(InetAddress targetAddr, short targetPort, short receivePort, int numPackets, int packetSize, int packetRate) {
+    this.targetAddr = targetAddr;
+    this.targetPort = targetPort;
+    this.receivePort = receivePort;
+    this.numPackets = numPackets;
+    this.packetSize = packetSize;
+    this.packetRate = packetRate;
+
+    // Create the array to store all of the packet data
+    packetData = new PacketData[numPackets];
+
+    // Create a latch to wait for all of the packets to be replied to
+    latch = new CountDownLatch(numPackets);
+  }
+
+  public void run() throws InterruptedException {
+    receivePackets();
+    packetTrain();
+
+    // Wait for all packets to return
+    latch.await();
+  }
+
+  public int getMaxBacklog() {
+    long[] sendTimestamps = new long[packetData.length];
+    long[] recvTimestamps = new long[packetData.length];
+    for (int i = 0; i < packetData.length; i++) {
+      sendTimestamps[i] = packetData[i].sendTime;
+      recvTimestamps[i] = packetData[i].receiveTime;
+    }
+
+    // Sort the send and receive timestamps so that they occur in order
+    Arrays.sort(sendTimestamps);
+    Arrays.sort(recvTimestamps);
+
+    // Iterate through the send and receive events
+    int backlog = 0, maxBacklog = 0;
+    int sendIndex = 0, recvIndex = 0;
+    while (sendIndex < packetData.length && recvIndex < packetData.length) {
+      // Calculate the relative timestamp. Not necessary but was useful for
+      // debugging
+      long sendTime = sendTimestamps[sendIndex] - sendTimestamps[0];
+      long recvTime = recvTimestamps[recvIndex] - sendTimestamps[0];
+      if (sendTime < recvTime) {
+        // If the next event was sending, increment the backlog and move
+        // to the next packet that was sent
+        sendIndex++;
+        backlog += packetSize;
+      } else if (sendTime > recvTime) {
+        // If the next event was receiving, decrement the backlog and move
+        // to the next packet that was received
+        recvIndex++;
+        backlog -= packetSize;
+      } else {
+        // If a send and receive occurred at the same time, then the backlog
+        // is unchanged but we should move to the next event for both sending
+        // and receiving
+        sendIndex++;
+        recvIndex++;
+      }
+
+      maxBacklog = Math.max(backlog, maxBacklog);
+    }
+
+    // The backlog at the end of the transmission should be 0
+    assert(backlog == 0);
+
+    return maxBacklog;
+  }
+
+  public void writeData(String filename) throws FileNotFoundException {
+    FileOutputStream fout =  new FileOutputStream(filename);
+    PrintStream pout = new PrintStream (fout);
+
+    // Analyze the data that was collected
+    PacketData first = null;
+    for (int i = 0; i < numPackets; i++) {
+      PacketData current = packetData[i];
+      if (first == null) first = current;
+
+      // Determine the sequence number, send time, and receive time (in microseconds relative
+      // to the send time of the first packet)
+      int seqNo = current.seqNo;
+      long sendTime = (current.sendTime - first.sendTime) / 1000;
+      long receiveTime = (current.receiveTime - first.sendTime) / 1000;
+
+      String line = seqNo + "\t" + sendTime + "\t" + receiveTime;
+      //System.out.println(line);
+      pout.println(line);
+    }
   }
 
   public static void main(String[] args) {
@@ -91,37 +194,10 @@ public class Estimator {
         int packetSize = Integer.parseInt(args[4]);
         int packetRate = Integer.parseInt(args[5]);
 
-        // Create the array to store all of the packet data
-        PacketData[] packetData = new PacketData[numPackets];
-
-        // Create a latch to wait for all of the packets to be replied to
-        CountDownLatch latch = new CountDownLatch(numPackets);
-
-        receivePackets(packetData, latch, numPackets, receivePort);
-        packetTrain(packetData, targetAddr, numPackets, packetSize, packetRate, targetPort, receivePort);
-
-        // Wait for all packets to return
-        latch.await();
-
-        FileOutputStream fout =  new FileOutputStream("packet_train.data");
-        PrintStream pout = new PrintStream (fout);
-
-        // Analyze the data that was collected
-        PacketData first = null;
-        for (int i = 0; i < numPackets; i++) {
-          PacketData current = packetData[i];
-          if (first == null) first = current;
-
-          // Determine the sequence number, send time, and receive time (in microseconds relative
-          // to the send time of the first packet)
-          int seqNo = current.seqNo;
-          long sendTime = (current.sendTime - first.sendTime) / 1000;
-          long receiveTime = (current.receiveTime - first.sendTime) / 1000;
-
-          String line = seqNo + "\t" + sendTime + "\t" + receiveTime;
-          System.out.println(line);
-          pout.println(line);
-        }
+        Estimator estimator = new Estimator(targetAddr, targetPort, receivePort, numPackets, packetSize, packetRate);
+        estimator.run();
+        estimator.writeData("packet_train.data");
+        System.out.println("Max backlog: " + estimator.getMaxBacklog());
       } catch (Exception e) {
         e.printStackTrace();
       }
--- lab4/Part1.m
+++ lab4/Part1.m
@@ -2,23 +2,49 @@
 [seqNo2, sendTime2, receiveTime2] = textread('packet_train_N_100_L_400_r_1000.data', '%f %f %f');
 [seqNo3, sendTime3, receiveTime3] = textread('packet_train_N_100_L_400_r_10000.data', '%f %f %f');
 
-figure(1);
-plot(seqNo1, sendTime1, 'r', seqNo1, receiveTime1, 'b')
+figure(1)
+stairs(seqNo1, sendTime1, 'r')
+hold
+stairs(seqNo1, receiveTime1, 'b')
 legend('Send Timestamps', 'Receive Timestamps')
 title('Send an Receive Timestamps vs. Sequence Number for N=100, L=400, r=10')
 xlabel('Sequence Number')
 ylabel('Timestamp (microseconds)')
 
-figure(2);
-plot(seqNo2, sendTime2, 'r', seqNo2, receiveTime2, 'b')
+figure(2)
+stairs(seqNo2, sendTime2, 'r')
+hold
+stairs(seqNo2, receiveTime2, 'b')
 legend('Send Timestamps', 'Receive Timestamps')
 title('Send an Receive Timestamps vs. Sequence Number for N=100, L=400, r=1000')
 xlabel('Sequence Number')
 ylabel('Timestamp (microseconds)')
 
-figure(3);
-plot(seqNo3, sendTime3, 'r', seqNo3, receiveTime3, 'b')
+figure(3)
+stairs(seqNo3, sendTime3, 'r')
+hold
+stairs(seqNo3, receiveTime3, 'b')
 legend('Send Timestamps', 'Receive Timestamps')
 title('Send an Receive Timestamps vs. Sequence Number for N=100, L=400, r=10000')
 xlabel('Sequence Number')
 ylabel('Timestamp (microseconds)')
+
+sendTime3 = sort(sendTime3);
+receiveTime3 = sort(receiveTime3);
+L=400;
+arrivals3 = zeros(length(sendTime3), 1);
+departures3 = zeros(length(receiveTime3), 1);
+arrivals3(1) = L;
+for i = 2:length(seqNo3)
+    arrivals3(i) = arrivals3(i-1) + L;
+    departures3(i) = departures3(i-1) + L;
+end
+
+figure(4)
+stairs(sendTime3, arrivals3, 'r')
+hold
+stairs(receiveTime3, departures3, 'b')
+legend('Cumulative Send Function', 'Cumulative Receive Function')
+title('Cumulative Send and Receive Functions vs. Time')
+xlabel('Timestamp (microseconds)')
+ylabel('Bytes')
